5e1fdd9a26d5851f213fd689dba0b83129883b33
tckz <at.tckz@gmail.com>
2012-06-28 18:05:28 +0900

    文字列展開間違ってる

--- app/helpers/wiki_astah_helper.rb
+++ app/helpers/wiki_astah_helper.rb
@@ -32,7 +32,7 @@ module WikiAstahHelper
 				path_public = File.expand_path(File.join([Rails.root, 'public']))
 				path_src = File.expand_path(File.join([path_public, @path]))
 				if self.shallow_path?(path_src, path_public)
-					raise "Invalid path: {@path}"
+					raise "Too shallow path: #{@path}"
 				end
 
 				FileUtils.mkdir_p(File.dirname(out))
